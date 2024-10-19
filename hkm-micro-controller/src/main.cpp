a665a5321dd973e5e860986fb450751ecec6228a
micahtilton <72249867+micahtilton@users.noreply.github.com>
2024-10-18 20:10:44 -1000

    arduino wifi setup

--- hkm-micro-controller/src/main.cpp
+++ hkm-micro-controller/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "SECRETS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASS);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected to WiFi");
+
+  postButtonState();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  postButtonState();
+  delay(1000); // Add a delay to prevent spamming the server
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void postButtonState()
+{
+  WiFiClient client;
+  if (client.connect(SERVER_URL, SERVER_PORT))
+  {
+    String postData = "{\"state\":\""; // Use the TOKEN defined in SECRETS.h
+    client.println("POST / HTTP/1.1");
+    client.println("Host: " SERVER_URL); // Use SERVER_URL as the host
+    client.println("Content-Type: application/json");
+    client.println("Authorization: Bearer " TOKEN); // Add TOKEN in the authorization header
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.print(postData);
+
+    Serial.println("Post request sent");
+  }
+  else
+  {
+    Serial.println("Connection to server failed");
+  }
+  client.stop();
 }
